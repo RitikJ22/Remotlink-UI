1269881ea1fe35914411d7ebb880a5fbe15e810e
Ritik <ritikjaiswal148@gmail.com>
2023-12-06 08:48:35 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+const Header = () => {
+    
+    
+
+
+  return (
+    <div className=' flex justify-between w-[71] h-[1440] flex-shrink-0 "border-r-1 border-solid border-rgba-35-31-193-0.10'>
+        
+        <div> Logo icon</div>
+        <div className='w-[219px] text-#003879 text-display-p3 font-poppins text-sm font-normal font-semibold leading-normal'> Indian Institute of Technlogy Mumbai</div>
+        <div> Search Bar</div>
+        <div>Notification</div>
+        <div>
+        Language
+        </div>
+        <div>User</div>
+
+    </div>
+
+ 
+
+
+    
+  )
+}
+
+export default Header;
